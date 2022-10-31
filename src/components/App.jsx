08e06b028f14fc3e93ef41d7ff9caefc19c15122
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2022-10-31 11:56:35 +0200

    m

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,123 @@
-import React, {Component} from "react";
+import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { theme } from "./theme";
+import { theme } from './theme';
 
-import FeedbackOptions from "./feedbackOptions/FeedbackOptions";
-import Section from "./section/Section";
-import Statistics from "./statistics/Statistics";
-import Notification from "./notification/Notification";
+import FeedbackOptions from './feedbackOptions/FeedbackOptions';
+import Section from './section/Section';
+import Statistics from './statistics/Statistics';
+import Notification from './notification/Notification';
 
-export class App extends Component {
-  static defaultProps = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export function App() {
+  const [goodFb, addGoodFb] = useState(0);
+  const [neutralFb, addNeutralFb] = useState(0);
+  const [badFb, addBadFb] = useState(0);
+
+  const onLeaveFeedback = (e) => {
+    switch (e) {
+      case 'goodFb':
+        addGoodFb(goodFb => goodFb + 1);
+        break;
+
+      case 'neutralFb':
+        addNeutralFb(neutralFb => neutralFb + 1);
+        break;
+
+      case 'badFb':
+        addBadFb(badFb => badFb + 1);
+        break;
+
+      default:
+        break;
+    }
   };
-  // static propTypes = {
-    
-  // }
-  state = {
-    good: this.props.good,
-    neutral: this.props.neutral,
-    bad: this.props.bad,
+
+  const countTotalFeedback = () => {
+    const values = Object.values({goodFb, neutralFb, badFb});
+    return values.reduce((acc, values) => acc + values, 0);
   };
 
-  onLeaveFeedback = arg => {
-    this.setState(prevState => { return { [arg]: prevState[arg] + 1 } });
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((goodFb / countTotalFeedback()) * 100);
   };
-  
-  countTotalFeedback = () => {
-    const values = (Object.values(this.state))
-    return values.reduce((acc, values) => acc + values, 0)
-  }
-  
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100)
-  }
 
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
+  return (
+    <ThemeProvider theme={theme}>
       <Section title="Please leave feedback">
-        <FeedbackOptions 
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.onLeaveFeedback} />
+        <FeedbackOptions
+          options={Object.keys({goodFb, neutralFb, badFb})}
+          onLeaveFeedback={onLeaveFeedback}
+        />
       </Section>
       <Section title="Statistics">
-        { this.countTotalFeedback() 
-          ? <Statistics 
-              good={this.state.good} 
-              neutral={this.state.neutral} 
-              bad={this.state.bad} 
-              total={this.countTotalFeedback()} 
-              positivePercentage={this.countPositiveFeedbackPercentage()} />
-          : <Notification message="No feedback given" /> }
+        {countTotalFeedback() ? (
+          <Statistics
+            good={goodFb}
+            neutral={neutralFb}
+            bad={badFb}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="No feedback given" />
+        )}
       </Section>
-      </ThemeProvider>
-    )
-  }
+    </ThemeProvider>
+  );
+}
 
-};
+// export class App extends Component {
+//   static defaultProps = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+//   // static propTypes = {
 
+//   // }
+//   state = {
+//     good: this.props.good,
+//     neutral: this.props.neutral,
+//     bad: this.props.bad,
+//   };
 
+//   onLeaveFeedback = arg => {
+//     this.setState(prevState => {
+//       return { [arg]: prevState[arg] + 1 };
+//     });
+//   };
 
+//   countTotalFeedback = () => {
+//     const values = Object.values(this.state);
+//     return values.reduce((acc, values) => acc + values, 0);
+//   };
 
-// goodFeedBack = () => {
-//   // const {value} = this.arg
-//   this.setState(prevState => ({
-//     good: prevState.good + 1,
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+//   };
+
+//   render() {
+//     return (
+//       <ThemeProvider theme={theme}>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.onLeaveFeedback}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {this.countTotalFeedback() ? (
+//             <Statistics
+//               good={this.state.good}
+//               neutral={this.state.neutral}
+//               bad={this.state.bad}
+//               total={this.countTotalFeedback()}
+//               positivePercentage={this.countPositiveFeedbackPercentage()}
+//             />
+//           ) : (
+//             <Notification message="No feedback given" />
+//           )}
+//         </Section>
+//       </ThemeProvider>
+//     );
 //   }
-//   ))
-// }
-
-// neutralFeedBack = () => {
-//   this.setState(prevState => ({
-//     neutral: prevState.neutral + 1
-//   }))
-// }
-
-// badFeedBack = () => {
-//   this.setState(prevState => ({
-//     bad: prevState.bad + 1
-//   }))
 // }
